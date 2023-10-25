9b7e07a5546388326dbb16711e7bd1b4e70b5f86
S564535 <S564535@nwmissouri.edu>
2023-10-25 16:28:11 -0500

    bonus added

---
+++ routes/computation.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+
+app.get('/computation', (req, res) => {
+  const queryX = parseFloat(req.query.x); 
+  const lastDigit = req.user.id % 10;
+  const randomValue = queryX || Math.random(); 
+  const result = Math.cosh(randomValue);
+
+  res.status(200).json({
+    result: `Math.cosh() applied to ${queryX ? queryX : 'random value'} is ${result.toFixed(2)}`,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
