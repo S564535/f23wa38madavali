3030c9233267fca1cab0659a65e420f7c8173712
S564535 <S564535@nwmissouri.edu>
2023-10-25 20:02:37 -0500

    bonus  updated

--- routes/computation.js
+++ routes/computation.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const port = 3000;
+
+var round1 = Math.random() * 100;
+var round2 = Math.random() * 200;
+var n1 = 0;
+var n2 = 0;
+var n3 = 0;
+
+router.get('/', function(req, res, next) {
+    n1 = Math.cos(round1);
+    n2 = Math.cos(round2);
+    n3 = Math.cos(round1);
+
+    res.send(`The Random Numbers are ${round1} and ${round2} <br><br> round applied is ${round1} is ${n1} <br><br> round applied is ${round2} is ${n2} <br><br> round applied is ${round1} is ${n3}`);
 
 
-router.get('/computation', (req, res) => {
-  const queryX = parseFloat(req.query.x); 
-  const lastDigit = req.user.id % 10;
-  const randomValue = queryX || Math.random(); 
-  const result = Math.cosh(randomValue);
-
-  res.status(200).json({
-    result: `Math.cosh() applied to ${queryX ? queryX : 'random value'} is ${result.toFixed(2)}`,
-  });
-});
-
-router.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = router;
