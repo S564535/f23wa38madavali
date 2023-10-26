c597490c1cf254d116849e83b1edb687cb9411c9
S564535 <S564535@nwmissouri.edu>
2023-10-25 19:26:11 -0500

    bonus updated

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const port = 3000;
 
 
-
-app.get('/computation', (req, res) => {
+router.get('/computation', (req, res) => {
   const queryX = parseFloat(req.query.x); 
   const lastDigit = req.user.id % 10;
   const randomValue = queryX || Math.random(); 
